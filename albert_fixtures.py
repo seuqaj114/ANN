9f552a790591c30bf47c369898693c7aa3988fa9
seuqaj114 <migjacques@hotmail.com>
2014-09-28 22:53:14 +0100

    nothing here

---
+++ albert_fixtures.py
@@ -0,0 +1,8 @@
+#	Image dimensions: 176x144
+#	Layer1 size: 25344
+#	Layer2 size: 65
+#	Layer3 size: 4
+
+network_geometry = [25344,65,4]
+epochs = 10
+sample_size = 50
---
+++ albert_train.py
@@ -0,0 +1,28 @@
+import sys
+import network
+
+from albert_fixtures import network_geometry, epochs, sample_size
+from utils.session import load_session, save_session
+
+import numpy as np
+
+if __name__ == "__main__":
+	net = network.Network(network_geometry)
+
+	if len(sys.argv) > 1:
+		if sys.argv[1] == "load":
+			net.weights, net.biases = load_session()
+			print "Parameters loaded"
+
+	for i in range(epochs):
+		for j in range(sample_size/10):
+			training_input = np.load("data/pics%s.npy" % (j))
+			training_output = np.load("data/output%s.npy" % (j))
+
+			training_set = zip(training_input,training_output)
+
+			net.bgd(training_set,2.0)
+
+		print "epoch %s complete" % i
+
+	save_session(net.weights,net.biases)

Important: backpropagation had an error, missing derivative term, now fixed!

albert_apply.py
@@ -31,12 +31,9 @@ if __name__ == "__main__":
 		img = Image.open("pics/captcha.jpg").convert("LA")
 		pic = [t[0]/255.0 for t in img.getdata()]
 
-		if net.apply(pic)[0] > 0.9:
-			print "Sim"
-		else:
-			print "Nao" 
+		print net.apply(pic) 
 
-		time.sleep(1)
+		time.sleep(2)
 
 	#l = np.load("data/pics3.npy")
 	#o = np.load("data/output3.npy")

network.py
@@ -50,7 +50,7 @@ class Network():
 		delta.append(a_mat[-1]-y)
 
 		for w,a in zip(self.weights[-1:0:-1],a_mat[-2:0:-1]):
-			delta.append(np.dot(w.transpose(),delta[-1]))
+			delta.append(np.dot(w.transpose(),delta[-1])*a*(1-a))
 
 		return delta[-1::-1], a_mat
 
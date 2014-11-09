f9bb31144fc245dfcd0b174e6a015704ed5d2da2
seuqaj114 <migjacques@hotmail.com>
2014-11-09 15:23:44 +0000

    K-means algorithm complete

--- kmeans.py
+++ kmeans.py
@@ -3,28 +3,43 @@ from random import random
 import sys
 from math import sqrt
 
-def euclid_distance(x,y):
-	return sqrt(x**2+y**2)
+def euclid_distance(x1,x2):
+	#	x1 and x2 must be array-like objects
+	return sqrt((x1[0]-x2[0])**2+(x1[1]-x2[1])**2)
 
-def binarize_pixel(pixel):
-	if pixel > 0.5:
-		return 1
-	else
-		return 0
-
-binarize_array = np.vectorize(binarize_pixel)
-
-def kmeans(image,width,height):
-	image = np.array(image)
-
-	bin_image = binarize_array(image)
+def kmeans(image,width,height,epochs,clusters=None):
 
 	if type(width) != int or type(height) != int:
 		return "ERROR - width and height must be integers!"
 
+	image = np.array(image)
+
+	dots = []
+	for i in range(len(image)):
+		for j in range(len(image[i])):
+			if image[i][j] < 140:
+				dots.append([i,j])
+
+	print "Dots: %s" % (len(dots))
+
 	n_clusters = width*height
 
-	clusters = [[random()*width,random()*height] for i in range(n_clusters)]
+	if not clusters:
+		clusters = [[random()*width,random()*height] for i in range(n_clusters)]
 
-	#	proximity_array stores the points closer to each cluster center
-	proximity_array
+	for j in range(epochs):
+		print j
+		location_array = np.array([[0,0]]*n_clusters)
+		count_array = [0]*n_clusters
+
+		for dot in dots:
+			distance_array = [euclid_distance(dot,cluster) for cluster in clusters]
+			min_index = distance_array.index(min(distance_array))
+			location_array[min_index] += np.array(dot)
+			count_array[min_index] += 1
+			#print count_array
+
+		clusters = [location/count for location, count in zip(location_array,count_array)]
+
+	return clusters
+

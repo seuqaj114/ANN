40d83e16ab9c6a703869e9cf6069d50e5df43e12
seuqaj114 <migjacques@hotmail.com>
2014-11-10 23:12:11 +0000

    Added single cell cropping; added abs to slope calculation, to avoid smaller than 0 minimums.

--- table_rec.py
+++ table_rec.py
@@ -12,6 +12,8 @@ Steps:
 	- downsample
 	- find clusters
 
+To do:
+	- remove noise before cropping around table
 """
 
 #	Only working for vertical tables by now!
@@ -23,8 +25,10 @@ print "rows: %s; columns: %s" % (ROWS,COLS)
 RATIO = min(float(ROWS)/COLS,float(COLS)/ROWS)
 
 main_image = Image.open("pics/table.jpg").convert("LA")
+print main_image.size
 
 main_pic = [t[0] for t in main_image.getdata()]
+
 
 MAX_GRAY = max(main_pic)
 MIN_GRAY = min(main_pic)
@@ -33,16 +37,15 @@ print "max: %s; min %s" % (MAX_GRAY,MIN_GRAY)
 print "average: %s" % ((MAX_GRAY+MIN_GRAY)/2)
 
 contrast_image = main_image.point(lambda x: 0 if x <= (MAX_GRAY+MIN_GRAY)/2 else 255)
-#contrast_image.show()
-
-print main_image.size
+contrast_image = contrast_image.resize((contrast_image.size[0]/4,contrast_image.size[1]/4))
+print contrast_image.size
+contrast_image.show()
 
 
 #	Look for a rectangle around the table in the image
-
-rect_center = (main_image.size[0]/2,main_image.size[1]/2)
-width = main_image.size[0]/2
-height = main_image.size[1]/2
+rect_center = (contrast_image.size[0]/2,contrast_image.size[1]/2)
+width = contrast_image.size[0]/2
+height = contrast_image.size[1]/2
 
 width_count_array = []
 
@@ -61,10 +64,10 @@ while rect_center[0]-(width/2+inc)*RATIO > 0:
 
 	inc+=1
 
-width_slope_array = [width_count_array[i]-width_count_array[i-1] for i in range(1,len(width_count_array))]
+width_slope_array = [abs(width_count_array[i]-width_count_array[i-1]) for i in range(1,len(width_count_array))]
 croped_width = width_slope_array.index(min(width_slope_array))
 
-#pyplot.plot(count_array)
+#pyplot.plot(width_count_array)
 #pyplot.show()
 
 height_count_array = []
@@ -72,9 +75,9 @@ height_count_array = []
 inc=0
 while rect_center[1]-(width/2+inc)> 0:
 	croped_image = contrast_image.crop(map(int,(
-		rect_center[0]-(width/2)*RATIO,
+		rect_center[0]-(width/2+croped_width)*RATIO,
 		rect_center[1]-(height/2+inc),
-		rect_center[0]+(width/2)*RATIO,
+		rect_center[0]+(width/2+croped_width)*RATIO,
 		rect_center[1]+(height/2+inc)
 		)))
 	croped_pic = [t[0] for t in croped_image.getdata()]
@@ -83,31 +86,92 @@ while rect_center[1]-(width/2+inc)> 0:
 
 	inc+=1
 
-height_slope_array = [height_count_array[i]-height_count_array[i-1] for i in range(1,len(height_count_array))]
+height_slope_array = [abs(height_count_array[i]-height_count_array[i-1]) for i in range(1,len(height_count_array))]
 croped_height = height_slope_array.index(min(height_slope_array))
 
-pyplot.plot(height_count_array)
-pyplot.show()
+
+
+#pyplot.plot(height_slope_array)
+#pyplot.show()
+
+print "croped_width: %s" % croped_width
+print "croped_height: %s" % croped_height
 
 #	Display cropped image
-
 croped_image = contrast_image.crop(map(int,(
 		rect_center[0]-(width/2+croped_width)*RATIO,
 		rect_center[1]-(height/2+croped_height),
 		rect_center[0]+(width/2+croped_width)*RATIO,
 		rect_center[1]+(height/2+croped_height)
 		)))
+#croped_image.show()
+
+#	Draw the center of the estimated positions of the numbers
+draw = ImageDraw.Draw(croped_image)
+for i in range(ROWS):
+	for j in range(COLS):
+		draw.ellipse((
+			(croped_image.size[0]/COLS)/2+j*(croped_image.size[0]/COLS)-4,
+			(croped_image.size[1]/ROWS)/2+i*(croped_image.size[1]/ROWS)-4,
+			(croped_image.size[0]/COLS)/2+j*(croped_image.size[0]/COLS)+4,
+			(croped_image.size[1]/ROWS)/2+i*(croped_image.size[1]/ROWS)+4
+		),fill=128)
+
 croped_image.show()
 
 
-
 """
-draw = ImageDraw.Draw(contrast_image)
-draw.rectangle((rect_center[0]-width/2*RATIO,rect_center[1]-height/2,rect_center[0]+width/2*RATIO,rect_center[1]+height/2),outline=128)
+	Below, using height as reference!
 """
 
-#contrast_image.show()
+#	Run ever smaller rectangles over a cell to isolate the number
+#	Just running over the top-left-most cell
 
+#	The (0,0) cell
+i = 0
+j = 0
 
+#	Set the search limits
+WIDTH_MIN = j*croped_image.size[0]/COLS
+WIDTH_MAX = croped_image.size[0]/COLS + j*croped_image.size[0]/COLS
+HEIGHT_MIN = i*croped_image.size[1]/ROWS
+HEIGHT_MAX = croped_image.size[1]/ROWS + i*croped_image.size[1]/ROWS
 
+#	Set search rectangle's geometry
+BASE_WIDTH = WIDTH_MAX - WIDTH_MIN
+BASE_HEIGHT = HEIGHT_MAX - HEIGHT_MIN
+RECT_CENTER = (WIDTH_MIN + BASE_WIDTH/2,HEIGHT_MIN+BASE_HEIGHT/2)
 
+#	Set rectangle's ratio ( > 0 )
+RECT_RATIO = float(BASE_WIDTH)/BASE_HEIGHT
+
+#	Search cicle, rectangles from outside to center
+cell_count_array = []
+for inc in range(BASE_HEIGHT/2):
+	croped_cell = croped_image.crop(map(int,(
+		RECT_CENTER[0]-(BASE_WIDTH/2-inc*RECT_RATIO),
+		RECT_CENTER[1]-(BASE_HEIGHT/2-inc),
+		RECT_CENTER[0]+(BASE_WIDTH/2-inc*RECT_RATIO),
+		RECT_CENTER[1]+(BASE_HEIGHT/2-inc)
+		)))
+
+	croped_cell_pic = [t[0] for t in croped_cell.getdata()]
+	cell_count_array.append(croped_cell_pic.count(0))
+	print cell_count_array[-1]
+
+#	Find minimum of number of pixels rate of change
+cell_slope_array = [abs(cell_count_array[i]-cell_count_array[i-1]) for i in range(1,len(cell_count_array))]
+cell_height = cell_slope_array.index(min(cell_slope_array))
+
+pyplot.plot(cell_slope_array)
+pyplot.show()
+
+#	Get croped cell (final)
+croped_cell = croped_image.crop(map(int,(
+		RECT_CENTER[0]-(BASE_WIDTH/2-cell_height*RECT_RATIO),
+		RECT_CENTER[1]-(BASE_HEIGHT/2-cell_height),
+		RECT_CENTER[0]+(BASE_WIDTH/2-cell_height*RECT_RATIO),
+		RECT_CENTER[1]+(BASE_HEIGHT/2-cell_height)
+		)))
+
+croped_cell.show()

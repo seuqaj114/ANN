4af714585c8f3c9651222ad4250ca2db643ae409
seuqaj114 <migjacques@hotmail.com>
2014-12-12 22:53:55 +0000

    Added comments. Started redoing the gaussian threshold filtering.

--- table_rec.py
+++ table_rec.py
@@ -180,141 +180,149 @@ def row_spectrum(image,threshold=3):
 
 
 
-def get_cell():
-	#	Only working for vertical tables by now!
+def get_cell(ROWS,COLS,dims=None):
+
+	"""
+		dims is a tuple like 
+		(top_left_corner_x,top_left_corner_y,bottom_right_corner_x,bottom_right_corner_y)
+	"""
 
 	#ROWS = int(sys.argv[1])
 	#COLS = int(sys.argv[2])
-	ROWS = 4
-	COLS = 2
 
 	print "rows: %s; columns: %s" % (ROWS,COLS)
 
-	RATIO = min(float(ROWS)/COLS,float(COLS)/ROWS)
+	#	If table's dimensions are not given
+	if dims==None:
+		#	Only working for vertical tables by now!
 
-	main_image = Image.open("../fig/table.jpg").convert("LA")
-	print main_image.size
+		RATIO = min(float(ROWS)/COLS,float(COLS)/ROWS)
 
-	main_pic = [t[0] for t in main_image.getdata()]
+		main_image = Image.open("../fig/table.jpg").convert("LA")
+		print main_image.size
 
-
-	MAX_GRAY = max(main_pic)
-	MIN_GRAY = min(main_pic)
-
-	print "max: %s; min %s" % (MAX_GRAY,MIN_GRAY)
-	print "average: %s" % ((MAX_GRAY+MIN_GRAY)/2)
-
-	contrast_image = main_image.point(lambda x: 0 if x <= (MAX_GRAY+MIN_GRAY)/2 else 255)
-	contrast_image = contrast_image.resize((contrast_image.size[0]/2,contrast_image.size[1]/2))
-	print contrast_image.size
-	contrast_image.show()
-
-	"""
-	contrast_image2 = main_image.point(lambda x: int((x-MIN_GRAY)*255.0/(MAX_GRAY-MIN_GRAY)))
-	contrast_image2 = contrast_image2.resize((contrast_image2.size[0]/4,contrast_image2.size[1]/4))
-	contrast_image2 = contrast_image2.filter(ImageFilter.CONTOUR)
-	contrast_pic2 = [t[0] for t in contrast_image2.getdata()]
-	MAX_GRAY2 = max(contrast_pic2)
-	MIN_GRAY2 = min(contrast_pic2)
-	contrast_image2 = contrast_image2.point(lambda x: 0 if x <= (MAX_GRAY+MIN_GRAY)/2 else 255)
-
-	contrast_image2.show()
-	contrast_image2 = contrast_image2.filter(ImageFilter.BLUR)
-	"""
+		main_pic = [t[0] for t in main_image.getdata()]
 
 
-	#	Look for a rectangle around the table in the image
-	rect_center = (contrast_image.size[0]/2,contrast_image.size[1]/2)
-	width = contrast_image.size[0]/2
-	height = contrast_image.size[1]/2
+		MAX_GRAY = max(main_pic)
+		MIN_GRAY = min(main_pic)
 
-	width_count_array = []
+		print "max: %s; min %s" % (MAX_GRAY,MIN_GRAY)
+		print "average: %s" % ((MAX_GRAY+MIN_GRAY)/2)
 
-	#for inc in range(600):
-	inc=0
-	while rect_center[0]-(width/2+inc)*RATIO > 0:
+		""" Applying gaussian threshold. """
+		MEAN = np.mean(np.array(main_pic))
+		STD = np.std(np.array(main_pic))
+
+		contrast_image = main_image.point(lambda x: 0 if x <= (MAX_GRAY+MIN_GRAY)/2 else 255)
+		contrast_image = contrast_image.resize((contrast_image.size[0]/2,contrast_image.size[1]/2))
+		print contrast_image.size
+		contrast_image.show()
+
+		"""
+		contrast_image2 = main_image.point(lambda x: int((x-MIN_GRAY)*255.0/(MAX_GRAY-MIN_GRAY)))
+		contrast_image2 = contrast_image2.resize((contrast_image2.size[0]/4,contrast_image2.size[1]/4))
+		contrast_image2 = contrast_image2.filter(ImageFilter.CONTOUR)
+		contrast_pic2 = [t[0] for t in contrast_image2.getdata()]
+		MAX_GRAY2 = max(contrast_pic2)
+		MIN_GRAY2 = min(contrast_pic2)
+		contrast_image2 = contrast_image2.point(lambda x: 0 if x <= (MAX_GRAY+MIN_GRAY)/2 else 255)
+
+		contrast_image2.show()
+		contrast_image2 = contrast_image2.filter(ImageFilter.BLUR)
+		"""
+
+
+		#	Look for a rectangle around the table in the image
+		rect_center = (contrast_image.size[0]/2,contrast_image.size[1]/2)
+		width = contrast_image.size[0]/2
+		height = contrast_image.size[1]/2
+
+		width_count_array = []
+
+		#for inc in range(600):
+		inc=0
+		while rect_center[0]-(width/2+inc)*RATIO > 0:
+			croped_image = contrast_image.crop(map(int,(
+				rect_center[0]-(width/2+inc)*RATIO,
+				rect_center[1]-(height/2),
+				rect_center[0]+(width/2+inc)*RATIO,
+				rect_center[1]+(height/2)
+				)))
+			croped_pic = [t[0] for t in croped_image.getdata()]
+			width_count_array.append(croped_pic.count(0))
+			print width_count_array[-1]
+
+			inc+=1
+
+		width_slope_array = [abs(width_count_array[i]-width_count_array[i-1]) for i in range(1,len(width_count_array))]
+		croped_width = width_slope_array.index(min(width_slope_array))
+
+		#pyplot.plot(width_count_array)
+		#pyplot.show()
+
+		height_count_array = []
+
+		inc=0
+		while rect_center[1]-(width/2+inc)> 0:
+			croped_image = contrast_image.crop(map(int,(
+				rect_center[0]-(width/2+croped_width)*RATIO,
+				rect_center[1]-(height/2+inc),
+				rect_center[0]+(width/2+croped_width)*RATIO,
+				rect_center[1]+(height/2+inc)
+				)))
+			croped_pic = [t[0] for t in croped_image.getdata()]
+			height_count_array.append(croped_pic.count(0))
+			print height_count_array[-1]
+
+			inc+=1
+
+		height_slope_array = [abs(height_count_array[i]-height_count_array[i-1]) for i in range(1,len(height_count_array))]
+		croped_height = height_slope_array.index(min(height_slope_array))
+
+
+
+		#pyplot.plot(height_slope_array)
+		#pyplot.show()
+
+		print "croped_width: %s" % croped_width
+		print "croped_height: %s" % croped_height
+
+		#	Display cropped image
 		croped_image = contrast_image.crop(map(int,(
-			rect_center[0]-(width/2+inc)*RATIO,
-			rect_center[1]-(height/2),
-			rect_center[0]+(width/2+inc)*RATIO,
-			rect_center[1]+(height/2)
-			)))
-		croped_pic = [t[0] for t in croped_image.getdata()]
-		width_count_array.append(croped_pic.count(0))
-		print width_count_array[-1]
+				rect_center[0]-(width/2+croped_width)*RATIO,
+				rect_center[1]-(height/2+croped_height),
+				rect_center[0]+(width/2+croped_width)*RATIO,
+				rect_center[1]+(height/2+croped_height)
+				)))
+		#croped_image.show()
 
-		inc+=1
+		#	Draw the center of the estimated positions of the numbers
+		"""
+		draw = ImageDraw.Draw(croped_image)
+		for i in range(ROWS):
+			for j in range(COLS):
+				draw.ellipse((
+					(croped_image.size[0]/COLS)/2+j*(croped_image.size[0]/COLS)-4,
+					(croped_image.size[1]/ROWS)/2+i*(croped_image.size[1]/ROWS)-4,
+					(croped_image.size[0]/COLS)/2+j*(croped_image.size[0]/COLS)+4,
+					(croped_image.size[1]/ROWS)/2+i*(croped_image.size[1]/ROWS)+4
+				),fill=128)
+		"""
+		
+		#print croped_image.size
 
-	width_slope_array = [abs(width_count_array[i]-width_count_array[i-1]) for i in range(1,len(width_count_array))]
-	croped_width = width_slope_array.index(min(width_slope_array))
-
-	#pyplot.plot(width_count_array)
-	#pyplot.show()
-
-	height_count_array = []
-
-	inc=0
-	while rect_center[1]-(width/2+inc)> 0:
-		croped_image = contrast_image.crop(map(int,(
-			rect_center[0]-(width/2+croped_width)*RATIO,
-			rect_center[1]-(height/2+inc),
-			rect_center[0]+(width/2+croped_width)*RATIO,
-			rect_center[1]+(height/2+inc)
-			)))
-		croped_pic = [t[0] for t in croped_image.getdata()]
-		height_count_array.append(croped_pic.count(0))
-		print height_count_array[-1]
-
-		inc+=1
-
-	height_slope_array = [abs(height_count_array[i]-height_count_array[i-1]) for i in range(1,len(height_count_array))]
-	croped_height = height_slope_array.index(min(height_slope_array))
+	#	If table's dimensions are given
+	else:
+		croped_image = main_image.crop(dims)
 
 
-
-	#pyplot.plot(height_slope_array)
-	#pyplot.show()
-
-	print "croped_width: %s" % croped_width
-	print "croped_height: %s" % croped_height
-
-	#	Display cropped image
-	croped_image = contrast_image.crop(map(int,(
-			rect_center[0]-(width/2+croped_width)*RATIO,
-			rect_center[1]-(height/2+croped_height),
-			rect_center[0]+(width/2+croped_width)*RATIO,
-			rect_center[1]+(height/2+croped_height)
-			)))
-	#croped_image.show()
-
-	#	Draw the center of the estimated positions of the numbers
-
-	"""
-	draw = ImageDraw.Draw(croped_image)
-	for i in range(ROWS):
-		for j in range(COLS):
-			draw.ellipse((
-				(croped_image.size[0]/COLS)/2+j*(croped_image.size[0]/COLS)-4,
-				(croped_image.size[1]/ROWS)/2+i*(croped_image.size[1]/ROWS)-4,
-				(croped_image.size[0]/COLS)/2+j*(croped_image.size[0]/COLS)+4,
-				(croped_image.size[1]/ROWS)/2+i*(croped_image.size[1]/ROWS)+4
-			),fill=128)
-	"""
 	croped_image.show()
-
-
-	"""
-		Below, using height as reference!
-	"""
 
 	#	Run ever smaller rectangles over a cell to isolate the number
 	#	Just running over the top-left-most cell
 
-	#for i in range(ROWS):
-	#	for j in range(COLS):
-	#	The (0,0) cell
-
-	#print croped_image.size
+	#	Using height as reference!
 
 	croped_cells = []
 	for i in range(ROWS):
